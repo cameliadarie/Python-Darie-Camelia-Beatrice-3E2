0950de8d400c94d926af8799de9bc727a4faaeba
cameliadarie <75748894+cameliadarie@users.noreply.github.com>
2021-12-14 21:30:17 +0200

    Update main.py

--- Tema/main.py
+++ Tema/main.py
@@ -1,26 +1,30 @@
 import os
 import sys
+import shutil
 
-
-def force(lista):
+def force(lista, deleted_files):
     for i in range(0, len(lista)):
         try:
             os.remove(lista[i])
+            deleted_files.append(lista[i])
         except:
             pass
 
-def i_mic(lista):
+
+def i_mic(lista, deleted_files):
     for i in range(0, len(lista)):
         print("Doriti sa stergeti acest fisier?")
-        raspuns=input()
-        if raspuns=='Y' or raspuns=='y':
+        raspuns = input()
+        if raspuns == 'Y' or raspuns == 'y':
             os.remove(lista[i])
+            deleted_files.append(lista[i])
         else:
             print("Acest fisier nu a fost sters")
 
-def recursive (lista):
-    for i in os.walk(lista):
-         print(i)
+
+def verbose(deleted_files):
+    for i in deleted_files:
+        print("S-a sters: " + i + "\n")
 
 
 def help():
@@ -32,48 +36,90 @@ def versions():
     f = open("versions.txt", "r")
     print(f.read())
 
+def recursive(lista):
+    for i in lista:
+     shutil.rmtree(i, ignore_errors=False, onerror=None)
+
+def dir(lista):
+    for i in lista:
+        os.rmdir(i)
+
+
+def imare(lista):
+    print("Doriti sa continuati?")
+    raspuns = input()
+    if(raspuns=="Y" or raspuns=="y"):
+        return 1
+    return 0
+
+def validare_comanda (lista_fisiere,lista_argumente):
+    argumente_posibile=["-r","-R","--recursive","-i","-I","--verbose","--help","--version","-f","--force","-d","--dir"]
+    for i in range (0,len(lista_argumente)):
+        if lista_argumente[i] not in argumente_posibile and "-f"  not in lista_argumente :
+            return 0
+    if(len(lista_fisiere)==0):
+        print("Nu ati dat niciun fisier")
+    return 1
+
+
 
 def citire_comanda():
-    comanda = input()
     deleted_files = []
     lista_fisiere = []
-    subs = comanda.split()
-    for i in range(1, len(subs)):
-        if subs[i][0] != '-':
-            lista_fisiere.append(subs[i])
-    # print(comanda)
-    if subs[0] == 'rm':
-        for i in range(1, len(subs)):
-            if subs[i] == '-f' or subs[i] == '--force':
-                force(lista_fisiere)
-            elif subs[i] == '-i':
-                i_mic(lista_fisiere)
-            elif subs[i] == '-I':
-                # force(subs(len(subs)-1))
-                a = 1
-            elif subs[i] == '-interactive[always]':
-                i_mic(lista_fisiere)
+    lista_argumente=[]
+    rec=0
+    for i in range(2, len(sys.argv[0:])):
+        a=str(sys.argv[i])
+        if a[0]!= '-' :
+            lista_fisiere.append(sys.argv[i])
+        else:
+            lista_argumente.append(sys.argv[i])
+        if a=="-r" or a=="--recursive" or a=="-R":
+            rec = 1
 
-            elif subs[i] == '--one-file-system':
+    if validare_comanda(lista_fisiere,lista_argumente)==0:
+        print("Argumentele date sunt invalide")
+
+    if sys.argv[1] == 'rm':
+        for i in range(1, len(sys.argv)):
+            if sys.argv[i] == '-f' or sys.argv[i] == '--force':
+                force(lista_fisiere, deleted_files)
+            elif sys.argv[i] == '-i':
+                i_mic(lista_fisiere, deleted_files)
+            elif sys.argv[i] == '-I':
+                if len(lista_fisiere)>3 or rec==1 :
+                    if(imare(lista_fisiere)==0):
+                        break
+
+            elif sys.argv[i].find("--interactive")>0:
+                print("da")
+                if sys.argv[i].find("always"):
+                    i_mic(lista_fisiere)
+                elif sys.argv[i].find("once"):
+                    imare(lista_fisiere)
+
+            elif sys.argv[i] == '--one-file-system':
                 # force(subs(len(subs)-1))
                 a = 1
-            elif subs[i] == '--no-preserve-root':
+            elif sys.argv[i] == '--no-preserve-root':
                 # force(subs(len(subs)-1))
                 a = 1
-            elif subs[i] == '-preserve - root[ = all]':
+            elif sys.argv[i] == '-preserve - root[ = all]':
                 # force(subs(len(subs)-1))
                 a = 1
-            elif subs[i] == '-r' or subs[i] == '-R' or subs[i] == '-recursive':
-                 recursive(lista_fisiere)
-            elif subs[i] == '-d' or subs[i] == '--dir':
-                # force(subs(len(subs)-1))
+            elif sys.argv[i] == '-r' or sys.argv[i] == '-R' or sys.argv[i] == '--recursive':
+                recursive(lista_fisiere)
+
+
+            elif sys.argv[i] == '-d' or sys.argv[i] == '--dir':
+                dir(lista_fisiere)
+
+            elif sys.argv[i] == '-v' or sys.argv[i] == '--verbose':
+                verbose(deleted_files)
                 a = 1
-            elif subs[i] == '-v' or subs[i] == '--verbose':
-                # force(subs(len(subs)-1))
-                a = 1
-            elif subs[i] == '--help':
+            elif sys.argv[i] == '--help':
                 help()
-            elif subs[i] == '--versions':
+            elif sys.argv[i] == '--versions':
                 versions()
 
 
